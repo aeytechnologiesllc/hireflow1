963b731c96cfcf9d9b0ccca738c646132172cfa0
shahzaib rehman <shahz@Mac.attlocal.net>
2026-03-28 02:35:32 -0400

    Remove disconnected legacy UI files

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -2,6 +2,5 @@ export { FloatingParticles, GradientOrbs } from "./FloatingParticles";
 export { AnimatedProgressRing } from "./AnimatedProgressRing";
 export { StorytellingLoader } from "./StorytellingLoader";
 export { MilestoneAnimation } from "./MilestoneAnimation";
-export { WelcomeAnimation } from "./WelcomeAnimation";
 export { EmpathyAnimation } from "./EmpathyAnimation";
 export { StaggeredBarsLoader } from "./StaggeredBarsLoader";
